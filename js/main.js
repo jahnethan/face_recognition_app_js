08e9b4b932760608b301d1cdebccd0014dc4b068
Jonathan <jrvasquez54@gmail.com>
2020-07-27 15:38:10 -0500

    fine

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const video = document.getElementById("video");
-let predictedAges = [];
+
 
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
@@ -27,45 +27,17 @@ function turnCameraOn()
     startVideo();
 }
 
-video.addEventListener("playing", () => {
-    const canvas = faceapi.createCanvasFromMedia(video);
-    document.body.append(canvas);
-  
-    const displaySize = { width: video.width, height: video.height };
-    faceapi.matchDimensions(canvas, displaySize);
-  
+video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
     setInterval(async () => {
-      const detections = await faceapi
-        .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-        .withFaceLandmarks()
-        .withFaceExpressions()
-        .withAgeAndGender();
-      const resizedDetections = faceapi.resizeResults(detections, displaySize);
-  
-      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-  
-      faceapi.draw.drawDetections(canvas, resizedDetections);
-      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
-  
-      const age = resizedDetections[0].age;
-      const interpolatedAge = interpolateAgePredictions(age);
-      const bottomRight = {
-        x: resizedDetections[0].detection.box.bottomRight.x - 50,
-        y: resizedDetections[0].detection.box.bottomRight.y
-      };
-  
-      new faceapi.draw.DrawTextField(
-        [`${faceapi.utils.round(interpolatedAge, 0)} years`],
-        bottomRight
-      ).draw(canvas);
-    }, 100);
-  });
-  
-  function interpolateAgePredictions(age) {
-    predictedAges = [age].concat(predictedAges).slice(0, 30);
-    const avgPredictedAge =
-      predictedAges.reduce((total, a) => total + a) / predictedAges.length;
-    return avgPredictedAge;
-  }
-  
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+      const resizedDetections = faceapi.resizeResults(detections, displaySize)
+      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+      faceapi.draw.drawDetections(canvas, resizedDetections)
+      faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+    }, 100)
+  })
